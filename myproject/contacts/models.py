1183712e033b699e61c3c37c050930de23a0962f
Nishanth Reddy <109732648+nishanthnagidi@users.noreply.github.com>
2023-05-25 16:17:05 +0530

    Add files via upload

---
+++ myproject/contacts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+from django.db import models
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
